3986935cee8e2163d895ae1f1e4d1b964c17a18c
Mark Daniels <mark_daniels1982@hotmail.com>
2015-01-07 07:25:14 -0800

    add parent test and class

--- parent.rb
+++ parent.rb
@@ -1,6 +1,26 @@
 require_relative 'grandparent'
 
-class Parent
+class Parent < Grandparent
+
+	@@children = 0
+    @@family = []
+
+
+    def initialize(name, age, gender)
+       super(name, age, gender)
+    end
+
+    def name
+    	@name
+    end
+
+    def age
+    	@age
+    end
+
+    def gender
+    	@gender
+    end
 
 end
 
--- spec/parent_spec.rb
+++ spec/parent_spec.rb
@@ -3,21 +3,25 @@ require 'spec_helper'
 describe Parent do
 
   before(:context) do
-    @parent = Parent.new("david",30,"male")
+    @parent = Parent.new(name,age,gender)
   end
 
 
   describe "Initialization" do
     it "is an instance of the Parent class" do
+      expect(@parent).to be_an_instance_of "Parent"
 
     end
     it "is assigned a name" do
+      expect(@name).to eq "name"
 
     end
     it "is assigned an age" do
+      expect(@age).to eq "age"
 
     end
     it "is assigned a gender" do
+      expect(gender).too eq "gender"
 
     end
   end
